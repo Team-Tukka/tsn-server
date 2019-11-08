4fdf6f08569a775d986e0dc80a71e70ed9b3873f
unknown <xuniqh@gmail.com>
2019-11-08 20:54:12 +0100

    Oprettet funktion til at tjekke brugerens token.

--- server.js
+++ server.js
@@ -1,22 +1,39 @@
-import express from "express";
-import connectDB from "./config/db";
-import typeDefs from "./modules/schemas";
-import resolvers from "./modules/resolvers";
-import { ApolloServer } from "apollo-server-express";
+import express from 'express';
+import connectDB from './config/db';
+import typeDefs from './modules/schemas';
+import resolvers from './modules/resolvers';
+import { ApolloServer } from 'apollo-server-express';
+import getUser from './auth/getUser';
 
-const server = new ApolloServer({
+/* Oprettelse af en ApolloServer instans, og inkludering af TypeDefinitions
+ * og Resolvers. Tilmed tilføjelse af token til Header Context
+ */
+const apolloServer = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: ({ req }) => {
+    const tokenWithBearer = req.headers.authorizztion || '';
+    const token = tokenWithBearer.split(' ')[1];
+    const user = getUser(token);
+
+    return {
+      user
+    };
+  }
 });
 
+// Instans af express
 const app = express();
-
-server.applyMiddleware({ app });
 
 // Connect Database
 connectDB();
 
+// Brug af express application som middleware til Apollo Serveren
+apolloServer.applyMiddleware({ app });
+
 // Sætter porten som express vil lytte på
 app.listen({ port: 3000 }, () => {
-  console.log(`Server kører på http://localhost:3000${server.graphqlPath}`);
+  console.log(
+    `Server kører på http://localhost:3000${apolloServer.graphqlPath}`
+  );
 });
